033801457af620ccd3b8b7fa95ad7ea8d82d1264
Piotr Romanowski <piotrromanowski1@gmail.com>
2021-12-28 14:55:05 -0500

    initial jlox template

---
+++ BUILD
@@ -0,0 +1,7 @@
+load("@rules_java//java:defs.bzl", "java_binary")
+
+java_binary(
+    name = "Lox",
+    srcs = glob(["src/main/java/com/lox/*.java"]),
+    main_class = "com.lox.Lox",
+)
---
+++ src/main/java/com/lox/Scanner.java
@@ -0,0 +1,12 @@
+package com.lox;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Scanner {
+  public Scanner() { }
+
+  public List<Token> scanTokens() {
+    return new ArrayList<Token>();
+  }
+}
